e19c1e8c6315476284096264482c3b8de778dbca
WhatATragedy <Alex-Ford@hotmail.co.uk>
2021-08-31 18:54:24 +0100

    Added filtering by genre endpoint and date filtering

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ import pandas
 import numpy as np
 import time
 import requests
+import re
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -17,6 +19,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # df = pandas.read_csv("archive/rotten_tomatoes_movies.csv")
 df = pandas.read_csv("archive/enriched.csv")
 
+@app.route('/movies/genres', methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def get_genres():
+    genres = set()
+    genre_tags = df['genres'].tolist()
+    for tag in genre_tags:
+        if not isinstance(tag, str):
+            continue
+        for genre in re.findall(r"[\w'& ]+", tag):
+            
+            genres.add(genre)
+    genres = list(genres)
+    print(len(genres))
+    return json.dumps(genres)
+
 @app.route('/reviews/movie/<movie_id>', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_movie_id(movie_id):
@@ -24,12 +41,20 @@ def get_movie_id(movie_id):
     print(rows)
     return json.dumps(rows.to_dict(orient="records"))
 
-@app.route('/reviews/genres/<genre>', methods=['GET'])
+@app.route('/reviews/genres/<genre>', methods=['GET', 'POST'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_genre_reviews(genre):
-    rows = df[df['genres'].str.contains(genre, na=False)]
-    respone_df = pandas.DataFrame(rows).replace({np.nan:None})
+    print(request.data)
+    if "date" in request.json:
+        rows = df[(df['genres'].str.contains(genre, na=False)) & (df['original_release_date'] >= request.json.get("date"))]
+        respone_df = pandas.DataFrame(rows).replace({np.nan:None})
+    else:
+        rows = df[df['genres'].str.contains(genre, na=False)]
+        respone_df = pandas.DataFrame(rows).replace({np.nan:None})
     return jsonify(respone_df.to_dict(orient="records"))
+
+    
+    
 
 def get_movie_image(movie_title, release_year):
     params = {
